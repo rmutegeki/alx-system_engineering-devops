a4b82c0ff33852e79916689ff0e9748dc81a4824
Ronald Mutegeki <murogive@gmail.com>
2023-01-11 03:28:53 -0800

    Add: 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest creates a file with permissions 0744, owned by www-data, and group www-data, and containing the text "I love Puppet"
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet",
+}
